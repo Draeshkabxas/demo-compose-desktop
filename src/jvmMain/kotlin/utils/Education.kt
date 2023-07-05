df20c704cacabfaa7ee6a35c492634ab3a0109fe
Ahmedaljhani <124582107+Ahmedaljhani@users.noreply.github.com>
2023-07-05 12:06:44 +0200

    add cancel btn and some editesbnnnn

---
+++ src/jvmMain/kotlin/utils/Education.kt
@@ -0,0 +1,13 @@
+package utils
+
+enum class Education(var arabicName: String) {
+    MASTER_DEGREE("ماجستير"),
+    BACHELOR_DEGREE("بكالوريوس"),
+    LICENSE("ليسنس"),
+    HIGH_INSTITUTE("معهد عالي"),
+    MIDDLE_INSTITUTE("معهد متوسط"),
+    HIGH_SCHOOL_DIPLOMA("شهادة ثانوية"),
+    PREPARATORY_CERTIFICATE("شهادة اعدادية"),
+    PRIMARY("إبتدائية");
+}
+

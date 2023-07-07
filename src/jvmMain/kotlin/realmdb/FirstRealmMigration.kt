1ba8aebb85efc0157a0a900e9a1648ba0eb3d21e
Draeshkabxas <Draeshkabxas>
2023-07-07 22:55:48 +0200

    fix realm migration problems

--- src/jvmMain/kotlin/realmdb/FirstRealmMigration.kt
+++ src/jvmMain/kotlin/realmdb/FirstRealmMigration.kt
@@ -1,11 +1,13 @@
 package realmdb
 
 
+import features.contracts.data.model.RealmContract
 import io.realm.kotlin.dynamic.DynamicMutableRealm
 import io.realm.kotlin.dynamic.DynamicMutableRealmObject
 import io.realm.kotlin.dynamic.DynamicRealm
 import io.realm.kotlin.dynamic.DynamicRealmObject
 import io.realm.kotlin.migration.AutomaticSchemaMigration
+import io.realm.kotlin.schema.RealmClass
 import org.mongodb.kbson.ObjectId
 
 fun firstRealmMigrate(): AutomaticSchemaMigration = AutomaticSchemaMigration { context ->
@@ -14,13 +16,17 @@ fun firstRealmMigrate(): AutomaticSchemaMigration = AutomaticSchemaMigration { c
 
     val currentVersion = oldRealm.schemaVersion()
     if (currentVersion == 0L) {
+        if (oldRealm.schema()["RealmContract"]?.isEmbedded == true) {
         context.enumerate("RealmContract") { oldObject: DynamicRealmObject, newObject: DynamicMutableRealmObject? ->
             val oldId = oldObject.getValue("id", String::class)
             newObject?.set("id", if (oldId.isEmpty()) ObjectId() else ObjectId(oldId))
         }
+        }
     }else if(currentVersion == 1L){
-        context.enumerate("RealmPerson") { oldObject: DynamicRealmObject, newObject: DynamicMutableRealmObject? ->
-            newObject?.set("ageGroup", "")
+        if (oldRealm.schema()["RealmPerson"]?.isEmbedded == true) {
+            context.enumerate("RealmPerson") { oldObject: DynamicRealmObject, newObject: DynamicMutableRealmObject? ->
+                newObject?.set("ageGroup", "")
+            }
         }
     }
 

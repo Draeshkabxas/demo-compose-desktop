950259aa1e0447af5f3ede7f383b2e465525688f
Ahmedaljhani <124582107+Ahmedaljhani@users.noreply.github.com>
2023-07-07 12:27:22 +0200

    11

---
+++ src/jvmMain/kotlin/features/contracts/presentation/contracts/component/PaginationTable.kt
@@ -0,0 +1,177 @@
+package features.contracts.presentation.contracts.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import features.contracts.domain.model.Contract
+import features.sons_of_officers.domain.model.Person
+import styles.CairoTypography
+
+@Composable
+fun PaginatedTable(
+    headers: List<String>,
+    personList: List<Contract>,
+    itemsPerPage: Int,
+    columnWidths: List<Dp>
+) {
+    if (personList.isEmpty()) return
+    val pageCount = (personList.size + itemsPerPage - 1) / itemsPerPage
+    var currentPage by remember { mutableStateOf(0) }
+    //btn check
+    var isButtonVisible by remember { mutableStateOf(false) }
+
+
+    Column() {
+        Row {
+            headers.forEachIndexed { index, header ->
+                Text(
+                    text = header,
+                    style = CairoTypography.h4,
+                    color = Color.White,
+                    modifier = Modifier
+                        .width(columnWidths[index])
+                        .background(Color(0xff3B5EA1))
+//                        .border(0.5.dp, Color.White)
+                        .padding(8.dp)
+                )
+            }
+        }
+        if (personList.isNullOrEmpty()) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) {
+                Text(text = "No data")
+            }
+        } else {
+            var counter = 0 // initialize counter based on current page and items per page
+        LazyColumn {
+            items(personList.chunked(itemsPerPage)[currentPage]) { person ->
+                Row(
+                    modifier = Modifier.background(
+                        if ((currentPage % 2 == 0 && personList.indexOf(person) % 2 == 0) ||
+                            (currentPage % 2 != 0 && personList.indexOf(person) % 2 != 0)
+                        )
+                            Color.LightGray else Color.White
+                    )
+                ) {
+                    Text(
+                        text = (personList.indexOf(person) + 1).toString(), // display counter value as text
+                        maxLines = 1,
+                        modifier = Modifier
+                            .width(columnWidths[0])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.fileNumber,
+                        modifier = Modifier
+                            .width(columnWidths[1])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.name,
+                        modifier = Modifier
+                            .width(columnWidths[2])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.libyaId,
+                        modifier = Modifier
+                            .width(columnWidths[3])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.motherName,
+                        modifier = Modifier
+                            .width(columnWidths[4])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.motherNationality,
+                        modifier = Modifier
+                            .width(columnWidths[5])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.educationLevel,
+                        modifier = Modifier
+                            .width(columnWidths[6])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.city,
+                        modifier = Modifier
+                            .width(columnWidths[7])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.phoneNumber,
+                        modifier = Modifier
+                            .width(columnWidths[8])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.dependency,
+                        modifier = Modifier
+                            .width(columnWidths[9])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.bankName,
+                        modifier = Modifier
+                            .width(columnWidths[10])
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = person.accountNumber,
+                        modifier = Modifier
+                            .width(columnWidths[11])
+                            .padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                shape = RoundedCornerShape(20.dp),
+                onClick = { if (currentPage > 0) currentPage-- }) {
+                Text("السابقة",
+                    style = CairoTypography.h4,
+                    fontWeight = FontWeight.Bold
+
+                )
+            }
+            Text("الصفحة ${currentPage + 1} من $pageCount",
+                style = CairoTypography.h4,
+                fontWeight = FontWeight.Bold,
+
+                modifier = Modifier.padding(8.dp))
+            Button(
+                shape = RoundedCornerShape(20.dp),
+                onClick = { if (currentPage < pageCount - 1) currentPage++ }) {
+                Text("التالية",
+                    style = CairoTypography.h4,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}

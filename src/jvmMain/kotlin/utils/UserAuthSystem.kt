b7e7433fdeb83200bfbbc0f6be30d252935184ae
Draeshkabxas <Draeshkabxas>
2023-07-17 11:24:58 +0200

    add edite 14

--- src/jvmMain/kotlin/utils/UserAuthSystem.kt
+++ src/jvmMain/kotlin/utils/UserAuthSystem.kt
@@ -3,6 +3,7 @@ package utils
 import authorization.domain.model.Jobs.*
 import authorization.domain.model.Systems
 import authorization.domain.model.User
+import org.koin.core.context.GlobalContext.get
 
 class UserAuthSystem{
     lateinit var currentUser:User
@@ -11,3 +12,6 @@ class UserAuthSystem{
     fun canEdit(): Boolean = setOf(SuperAdmin,Admin).contains(currentUser.job)
     fun canChangeAccountsPermission():Boolean = currentUser.job == SuperAdmin
 }
+
+
+fun getUserAuth():UserAuthSystem = get().get<UserAuthSystem>()

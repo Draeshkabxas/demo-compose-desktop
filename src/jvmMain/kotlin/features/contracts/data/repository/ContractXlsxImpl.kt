4d0b3757c7dd3b434cd11b072e6101435fff11c7
derar <zzxxccz908@gmail.com>
2023-08-13 23:52:28 +0200

    add edit 48

--- src/jvmMain/kotlin/features/contracts/data/repository/ContractXlsxImpl.kt
+++ src/jvmMain/kotlin/features/contracts/data/repository/ContractXlsxImpl.kt
@@ -10,56 +10,72 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import utils.AgeGroup
+import utils.getAgeGroupFromLibyaId
 import java.io.FileOutputStream
 
-class ContractXlsxImpl: ContractXlsxRepository {
+class ContractXlsxImpl : ContractXlsxRepository {
 
-    override fun printContractsToXlsxFile(contracts: List<Contract>, filePath:String, headers:List<String>): Flow<Boolean> = flow {
+    override fun printContractsToXlsxFile(
+        contracts: List<Contract>,
+        filePath: String,
+        headers: List<String>
+    ): Flow<Boolean> = flow {
         val workbook = XSSFWorkbook()
         try {
             val sheet = workbook.createSheet("Persons")
             val headerRow = sheet.createRow(0)
-            headers.forEachIndexed{ index: Int, headerName: String ->
+            headers.forEachIndexed { index: Int, headerName: String ->
                 headerRow.createCell(index).setCellValue(headerName)
             }
             // ... create other header cells
             contracts.forEachIndexed { index, contract ->
                 val row = sheet.createRow(index + 1)
                 headers.forEachIndexed { cellIndex: Int, headerName: String ->
-                    when(headerName.trim()){
+                    when (headerName.trim()) {
                         "رقم الملف" -> {
                             row.createCell(cellIndex).setCellValue(contract.fileNumber)
                         }
+
                         "الاسم رباعي" -> {
                             row.createCell(cellIndex).setCellValue(contract.name)
                         }
+
                         "اسم الام" -> {
                             row.createCell(cellIndex).setCellValue(contract.motherName)
                         }
+
                         "المؤهل العلمي" -> {
                             row.createCell(cellIndex).setCellValue(contract.educationLevel)
                         }
+
                         "المدينة" -> {
                             row.createCell(cellIndex).setCellValue(contract.city)
                         }
+
                         "رقم الهاتف" -> {
                             row.createCell(cellIndex).setCellValue(contract.phoneNumber)
                         }
+
                         "جنسية الام" -> {
                             row.createCell(cellIndex).setCellValue(contract.motherNationality)
                         }
-                         "التبعية" -> {
+
+                        "التبعية" -> {
                             row.createCell(cellIndex).setCellValue(contract.dependency)
                         }
-                        "اسم المصرف"-> {
+
+                        "اسم المصرف" -> {
                             row.createCell(cellIndex).setCellValue(contract.bankName)
                         }
-                        "رقم الحساب"-> {
+
+                        "رقم الحساب" -> {
                             row.createCell(cellIndex).setCellValue(contract.accountNumber)
                         }
-                        "الرقم الوطني"-> {
+
+                        "الرقم الوطني" -> {
                             row.createCell(cellIndex).setCellValue(contract.libyaId)
                         }
+
                         else -> {}
                     }
                 }
@@ -77,17 +93,23 @@ class ContractXlsxImpl: ContractXlsxRepository {
     }.flowOn(Dispatchers.IO)
 
 
-    override fun getContractsFromXlsxFile(filePath: String): Flow<List<Contract>> = flow{
+    override fun getContractsFromXlsxFile(filePath: String): Flow<List<Contract>> = flow {
         val map: Map<String, (Contract, String) -> Contract> = mapOf(
-   "رقم الملف" to { contract: Contract, value: String -> contract.copy(fileNumber = value) },
-        "الاسم" to { contract: Contract, value: String -> contract.copy(name = value) },
-        "اسم الام" to { contract: Contract, value: String -> contract.copy(motherName = value) },
-        "جنسية الام" to { contract: Contract, value: String -> contract.copy(motherNationality = value) },
-        "الرقم الوطني" to { contract: Contract, value: String -> contract.copy(libyaId = value) },
-        "رقم الهاتف" to { contract: Contract, value: String -> contract.copy(phoneNumber = value) },
-        "التبعية" to { contract: Contract, value: String -> contract.copy(dependency = value) },
-        "الملاحظات" to { contract: Contract, value: String -> contract.copy(notes = value) },
-        "المؤهل العلمي" to { contract: Contract, value: String -> contract.copy(educationLevel = value) }
+            "رقم الملف" to { contract: Contract, value: String -> contract.copy(fileNumber = value) },
+            "الاسم" to { contract: Contract, value: String -> contract.copy(name = value) },
+            "اسم الام" to { contract: Contract, value: String -> contract.copy(motherName = value) },
+            "المدينة" to { contract: Contract, value: String -> contract.copy(city = value.filter { it.isLetter() }) },
+            "جنسية الام" to { contract: Contract, value: String -> contract.copy(motherNationality = value) },
+            "الرقم الوطني" to { contract: Contract, value: String ->
+                contract.copy(
+                    libyaId = value,
+                    ageGroup = getAgeGroupFromLibyaId(value)
+                )
+            },
+            "رقم الهاتف" to { contract: Contract, value: String -> contract.copy(phoneNumber = value) },
+            "التبعية" to { contract: Contract, value: String -> contract.copy(dependency = value) },
+            "الملاحظات" to { contract: Contract, value: String -> contract.copy(notes = value) },
+            "المؤهل العلمي" to { contract: Contract, value: String -> contract.copy(educationLevel = value) }
         )
         val contracts = xlsxToListOf(filePath, {
             Contract("", "", "", "", "", "", "", "", "", "", "", "", AgeGroup.UnderEightTeen, "")
